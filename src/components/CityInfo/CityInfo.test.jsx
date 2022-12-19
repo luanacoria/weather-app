18e7da21585da09186f561289ecb7bfea51ce3c5
OnaLuanaCoria <onaluanacoria@gmail.com>
2022-12-19 04:19:10 -0300

    first commit :)

---
+++ src/components/CityInfo/CityInfo.test.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import CityInfo from "./CityInfo";
+
+test("cityInfo render", async () => {
+	//Arrange
+	//Act
+    //Assert
+    const city = "Buenos Aires"
+    const country = "Argentina" 
+
+    const {findAllByRole} = render(<CityInfo city={city} country={country}></CityInfo>)
+    const cityAndCountryComponent = await findAllByRole("heading")
+
+    expect(cityAndCountryComponent[0]).toHaveTextContent(city)
+    expect(cityAndCountryComponent[1]).toHaveTextContent(country)
+});
