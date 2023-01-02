4c03ac2e227a42fba11327ed45a610f8477ac714
OnaLuanaCoria <onaluanacoria@gmail.com>
2023-01-02 14:37:01 -0300

    FEA: propType oneOf to validate states

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -10,6 +10,14 @@ import {
 } from "react-icons/wi";
 import { IconContext } from "react-icons";
 
+const validValues = [ 
+	"cloud",
+	"cloudy",
+	"fog",
+	"sunny",
+	"rain"
+]
+
 const stateByName = {
 	cloud: WiCloud,
 	cloudy: WiDayCloudy,
@@ -38,7 +46,7 @@ const Weather = ({ temperature, state }) => {
 
 Weather.propTypes = {
 	temperature: PropTypes.number.isRequired,
-	state: PropTypes.string.isRequired,
+	state: PropTypes.oneOf(validValues).isRequired,
 };
 
 export default Weather;
