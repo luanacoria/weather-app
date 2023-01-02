4f1688280b79f4b8d3d74680717e4ec64b9f6f66
OnaLuanaCoria <onaluanacoria@gmail.com>
2023-01-02 18:01:34 -0300

    FEA: evento click en CityList.stories

--- src/components/CityList/CityList.stories.jsx
+++ src/components/CityList/CityList.stories.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CityList from "./CityList";
+import { action } from "@storybook/addon-actions";
 
 export default {
 	title: "CityList",
@@ -25,4 +26,4 @@ const cities = [
 	},
 ];
 
-export const CityListExample = () => <CityList cities={cities} />;
+export const CityListExample = () => <CityList cities={cities} onClickCity={action("Click en city")}/>;
