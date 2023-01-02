69244bbc57008263bb7ede84786e77f38ed6bfa0
OnaLuanaCoria <onaluanacoria@gmail.com>
2023-01-02 15:48:24 -0300

    FEA: CityList event 'OnClickCity'

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Grid from '@mui/material/Grid'
-import CityInfo from '../CityInfo'
-import Weather from "./../Weather"
+import Grid from "@mui/material/Grid";
+import CityInfo from "../CityInfo";
+import Weather from "./../Weather";
 
-const renderCityAndCountry = cityAndCountry => {
+//renderCityAndCountry se va a convertir en una función que retorna otra función.
+
+const renderCityAndCountry = eventOnClickCity => (cityAndCountry) => {
 	const { city, country } = cityAndCountry;
 
 	return (
-		<li key={city}>
+		<li key={city} onClick={eventOnClickCity}>
 			<Grid container justifyContent="center" alignItems="center">
 				<Grid item xs={12} md={8}>
-					<CityInfo  city={city} country={country} />
+					<CityInfo city={city} country={country} />
 				</Grid>
 				<Grid item xs={12} md={4}>
 					<Weather temperature={10} state="sunny" />
@@ -21,16 +23,18 @@ const renderCityAndCountry = cityAndCountry => {
 	);
 };
 
-const CityList = ({ cities }) => {
+const CityList = ({ cities, onClickCity }) => {
 	return (
 		<ul>
-			{cities.map((cityAndCountry) => renderCityAndCountry(cityAndCountry))}
+			{cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry)
+			)}
 		</ul>
 	);
 };
 
 CityList.propTypes = {
 	cities: PropTypes.array.isRequired,
+	onClickCity: PropTypes.func.isRequired,
 };
 
 export default CityList;
