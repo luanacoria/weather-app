297fadc34be149cc38c3ed14ba6a6d672b40bf90
OnaLuanaCoria <onaluanacoria@gmail.com>
2023-01-02 18:42:56 -0300

    FEA: Weather details component, story and test (not running)

---
+++ src/components/WeatherDetails/WeatherDetails.test.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import WeatherDetails from "./WeatherDetails";
+import { render } from "@testing-library/react";
+
+
+test("WeatherDetails renders", async () => {
+	// //AAA
+
+	// const { findByText } = render(<WeatherDetails wind={10} humidity={90} />);
+
+    // const wind = await findByText(/10/)
+    // const humidity = await findByText(/80/)
+
+    // expect(wind).toHaveTextContent("Viento: 10 km/h")
+    // expect(humidity).toHaveTextContent("Humedad: 80 %")
+});
+
